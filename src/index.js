c08c3fb4c98bc99f0b92a66f60a25c9177271ada
sunaliseth <sunaliseth01@gmail.com>
2018-01-27 14:39:52 +0000

    CalculateWinner function added to the Board

--- src/index.js
+++ src/index.js
@@ -24,8 +24,10 @@ class Board extends React.Component {
     const squares =
     this.state.squares.slice();
     squares[i] = this.state.xIsNext ? 'O' : 'X'
-    this.setState({squares: squares,
-    xIsNext: !this.state.xIsNext});
+    this.setState({
+      squares: squares,
+    xIsNext: !this.state.xIsNext
+    });
   }
   renderSquare(i) {
     return <Square
@@ -35,8 +37,15 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player:' +
-      (this.state.xIsNext ? 'O' : 'X')
+    const winner =
+      calculateWinner(this.state.squares);
+    let status;
+      if (winner) {
+        status = "Winner: " + winner;
+      }else{
+        'Next player: ' +
+         (this.state.xIsNext ? 'O' : 'X')
+      }
 
     return (
       <div>
@@ -61,7 +70,32 @@ class Board extends React.Component {
   }
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+
+  for (let i = 0; i < lines.length; i++)
+  {
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b]
+      && squares[a] === squares[c])
+  {
+      return squares[a];
+    }
+  }
+    return null;
+
+}
 class Game extends React.Component {
+
   render() {
     return (
       <div className="game">
